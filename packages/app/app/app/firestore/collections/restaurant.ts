a81a3d9b281f45dd69c12ea30c593fa79b94b434
Martin Juul <code@juul.xyz>
2000-12-21 23:45:00 +0100

    refactor(FirebaseModels): use string maps for internationalized fields

--- packages/app/app/app/firestore/collections/restaurant.ts
+++ packages/app/app/app/firestore/collections/restaurant.ts
@@ -1,9 +1,22 @@
 import * as Firestore from "@react-native-firebase/firestore"
+import { StringMap } from "../../interfaces"
 
 export interface RestaurantModel {
-  id: number
+  id?: string
   name: string
   type: string
-  image: string
+  image?: string
   location: Firestore.FirebaseFirestoreTypes.GeoPoint
+}
+
+export interface RestaurantMenuItem {
+  id?: string
+  name: string
+  // key=ISO 639-1 value=language specific description
+  description: StringMap
+  image?: string
+  price: number
+  // key=ISO 639-1 value=language specific description
+  type: StringMap
+  productCode?: string
 }
