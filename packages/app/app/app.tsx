77a923e9faa0ef308cabe4bb4c169a42e0643380
Martin Juul <code@juul.xyz>
2000-12-21 23:45:00 +0100

    refactor(bootstrap): move bootstrapping to own file

--- packages/app/app/app.tsx
+++ packages/app/app/app.tsx
@@ -1,20 +1,22 @@
 import "./i18n"
 import "./utils/ignore-warnings"
-import React, { useEffect, useRef } from "react"
-import "./bootstrap"
+import React, { useEffect, useRef, useState } from "react"
 import { NavigationContainerRef } from "@react-navigation/native"
 import { SafeAreaProvider, initialWindowMetrics } from "react-native-safe-area-context"
-import { initFonts } from "./theme/fonts"
 import { Provider } from "react-redux"
 import { store } from "./store"
 import * as storage from "./utils/storage"
 import { useBackButtonHandler, RootNavigator, canExit, setRootNavigation, useNavigationPersistence } from "./navigators"
 import { PermissionsProvider } from "./contexts/permissions"
 import { LanguageProvider } from "./contexts/language"
+import { bootstrap } from "./bootstrap"
+import { Text } from "./components"
 
 export const NAVIGATION_PERSISTENCE_KEY = "NAVIGATION_STATE"
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true)
+
   const navigationRef = useRef<NavigationContainerRef<any>>()
   setRootNavigation(navigationRef)
   useBackButtonHandler(navigationRef, canExit)
@@ -26,9 +28,16 @@ function App() {
   // Kick off initial async loading actions, like loading fonts and RootStore
   useEffect(() => {
     ;(async () => {
-      await initFonts()
+     //  await initFonts()
+      await bootstrap()
+
+      setIsLoading(true)
     })()
   }, [])
+
+  if (isLoading) {
+    <Text>Loading...</Text>
+  }
 
   // otherwise, we're ready to render the app
   return (
--- packages/app/app/bootstrap.ts
+++ packages/app/app/bootstrap.ts
@@ -1,3 +1,16 @@
 import { enableScreens } from "react-native-screens"
+import { GoogleSignin } from "@react-native-google-signin/google-signin"
+import { Platform } from "react-native"
 
-enableScreens()
+export async function bootstrap() {
+  enableScreens()
+
+  const webClientId = Platform.select({
+    android: "81702780710-koheumf9pu03q6edihbu6sg1upnob879.apps.googleusercontent.com",
+    ios: "81702780710-m637qgdc0dvd67db271bt5ctfeeuoo2b.apps.googleusercontent.com",
+  })
+
+  GoogleSignin.configure({
+    webClientId,
+  })
+}
