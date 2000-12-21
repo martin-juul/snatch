6eb4cbc56f0b45f0fb21aaab5ac7b938eac57798
Martin Juul <code@juul.xyz>
2000-12-21 23:45:00 +0100

    refactor(orders-screen): use navigation types

--- packages/app/app/screens/orders/orders-screen.tsx
+++ packages/app/app/screens/orders/orders-screen.tsx
@@ -3,12 +3,16 @@ import { FlatList, Pressable, ViewStyle } from "react-native"
 import { Screen, Text } from "../../components"
 import { Orders } from "./mock-data"
 import FastImage from "react-native-fast-image"
+import { OrderProps, OrderRoute } from "../../navigators/order"
 
 const ROOT: ViewStyle = {
   flex: 1,
 }
 
-export const OrdersScreen: React.FC = () => {
+interface Props extends OrderProps<OrderRoute.List> {
+}
+
+export const OrdersScreen = ({ navigation }: Props) => {
   return (
     <Screen testID="OrdersScreen" style={ROOT} preset="fixed">
       <Text preset="header" text="Orders" />
@@ -17,7 +21,10 @@ export const OrdersScreen: React.FC = () => {
         keyExtractor={(item) => item.id.toString()}
         data={[...Orders]}
         renderItem={({ item }) => (
-          <Pressable style={{ flexDirection: "row" }}>
+          <Pressable
+            style={{ flexDirection: "row" }}
+            onPress={() => navigation.navigate(OrderRoute.Tracking, { id: item.id })}
+          >
             <FastImage
               style={{ width: 100, height: 100 }}
               source={{ uri: item.items[0].image }}
