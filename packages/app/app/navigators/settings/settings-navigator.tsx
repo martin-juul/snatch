0253a240a9ebd37f9e51d41d8988a857b099b48e
Martin Juul <code@juul.xyz>
2000-12-21 23:45:00 +0100

    refactor(SettingsNavigator): added Language route and typed props

--- packages/app/app/navigators/settings/settings-navigator.tsx
+++ packages/app/app/navigators/settings/settings-navigator.tsx
@@ -1,19 +1,40 @@
-import React from 'react'
-import { createStackNavigator } from "@react-navigation/stack"
-import { SettingsScreen } from "../../screens"
+import React from "react"
+import { createStackNavigator, StackNavigationProp } from "@react-navigation/stack"
+import { SettingsLanguageScreen, SettingsScreen } from "../../screens"
+import { RouteProp } from "@react-navigation/native"
+import { translate } from "../../i18n"
 
 export enum SettingsRoute {
-  List = "Settings"
+  List = "Settings",
+  Language = "Language",
 }
 
 export type SettingsParamList = {
   [SettingsRoute.List]: undefined
+  [SettingsRoute.Language]: undefined
+}
+
+export type SettingsNavigationProp<RouteName extends keyof SettingsParamList> = StackNavigationProp<SettingsParamList, RouteName>
+export type SettingsRouteProp<RouteName extends keyof SettingsParamList> = RouteProp<SettingsParamList, RouteName>
+
+export interface SettingsProps<RouteName extends keyof SettingsParamList> {
+  navigation: SettingsNavigationProp<RouteName>
+  route: SettingsRouteProp<RouteName>
 }
 
 const { Navigator, Screen } = createStackNavigator<SettingsParamList>()
 
 export const SettingsNavigator = () => (
   <Navigator>
-    <Screen name={SettingsRoute.List} component={SettingsScreen} />
+    <Screen
+      name={SettingsRoute.List}
+      component={SettingsScreen}
+      options={{ headerBackTitle: translate("common.back") }}
+    />
+
+    <Screen
+      name={SettingsRoute.Language}
+      component={SettingsLanguageScreen}
+    />
   </Navigator>
 )
