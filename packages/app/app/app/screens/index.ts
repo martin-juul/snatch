f250e176411c8bcb134c9deeb1bc61f7af8257d9
Martin Juul <code@juul.xyz>
2000-12-21 23:45:00 +0100

    fix(screens): fix export for restaurant-detail-screen

--- packages/app/app/app/screens/index.ts
+++ packages/app/app/app/screens/index.ts
@@ -3,5 +3,5 @@ export * from './orders/orders-screen';
 export * from './orders/order-detail-screen';
 export * from './restaurant/restaurant-item-screen';
 export * from './restaurant/restaurant-list-screen';
-export * from './restaurant/restaurant-screen';
+export * from './restaurant/restaurant-detail-screen';
 // export other screens here
