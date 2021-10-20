f7f48bb00c56c2b11fff627e1ad34afd8456839f
Martin Juul <code@juul.xyz>
2021-10-20 13:37:43 +0200

    feat(icons): added icons to android & ios

--- packages/app/babel.config.js
+++ packages/app/babel.config.js
@@ -1,3 +1,24 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["module:metro-react-native-babel-preset"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["."],
+          extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
+          alias: {
+            components: "./app/components",
+            constant: "./app/constant",
+            ducks: "./app/ducks",
+            interfaces: "./app/interfaces",
+            navigation: "./app/navigation",
+            style: "./app/style",
+            utils: "./app/utils",
+          },
+        },
+      ],
+    ],
+  };
 };
